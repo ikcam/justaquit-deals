e8b4898278da655a393e4277f4f83d57c7cfc5ee
Irving Kcam <xploit29@gmail.com>
2012-10-23 17:12:55 -0500

    Relocate failes

--- main.php
+++ main.php
@@ -13,11 +13,11 @@ require_once('classes/Transaction.php');
 
 // Views
 require_once('includes/Box.php');
-require_once('includes/Main.php');
-require_once('includes/Orders.php');
-require_once('includes/Coupons.php');
-require_once('includes/Transactions.php');
-require_once('includes/Settings.php');
+require_once('includes/admin/Main.php');
+require_once('includes/admin/Orders.php');
+require_once('includes/admin/Coupons.php');
+require_once('includes/admin/Transactions.php');
+require_once('includes/admin/Settings.php');
 
 $deals_box = new Box();
 $page = new Deals();

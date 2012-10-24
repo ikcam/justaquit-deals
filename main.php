d5e0d047f6adcc788d1f4a90ae553966e9724cb9
Irving Kcam <xploit29@gmail.com>
2012-10-23 22:19:09 -0500

    Add new pages

--- main.php
+++ main.php
@@ -13,16 +13,25 @@ require_once('classes/Transaction.php');
 
 // Views
 require_once('includes/Box.php');
+
+// Admin Pages
 require_once('includes/admin/Main.php');
 require_once('includes/admin/Orders.php');
 require_once('includes/admin/Coupons.php');
 require_once('includes/admin/Transactions.php');
 require_once('includes/admin/Settings.php');
 
-$deals_box = new Box();
-$page = new Deals();
-$page = new Orders();
-$page = new Transactions();
-$page = new Coupons();
-$page = new Settings();
+// Shortcodes
+require_once('includes/shortcodes/transaction.php');
+require_once('includes/shortcodes/checkout.php');
+require_once('includes/shortcodes/ipn.php');
+require_once('includes/shortcodes/details.php');
+
+// Load
+$deals_box  = new Box();
+$deals_page = new Deals();
+$deals_page = new Orders();
+$deals_page = new Transactions();
+$deals_page = new Coupons();
+$deals_page = new Settings();
 ?>

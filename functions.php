a986a96063228c1f978ec3e4e6e8f0e68d8957f5
Irving Kcam <xploit29@gmail.com>
2013-01-10 10:35:16 -0500

    Function to Get data from URL

--- functions.php
+++ functions.php
@@ -426,4 +426,15 @@ function buys_count($ID){
 
 	add_post_meta($ID, '_product_buys', $count, true) or update_post_meta($ID, '_product_buys', $count);
 }
+
+function get_data( $url ){
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
 ?>

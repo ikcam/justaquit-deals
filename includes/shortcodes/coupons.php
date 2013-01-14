a44de18535bb7d63cddf5d423e32fca92a1e2e7c
Irving Kcam <xploit29@gmail.com>
2013-01-14 18:41:50 -0500

    Coupon

--- includes/shortcodes/coupons.php
+++ includes/shortcodes/coupons.php
@@ -10,31 +10,41 @@ Class shortcode_coupons{
 	}
 
 	public function shortcode(){
-		if( isset($_POST['submit']) && isset($_GET['coupon']) ):
-			
+		if( isset($_POST['submit']) ):
+			// Pass: Usage coupon page
 
-		elseif( isset($_GET['coupon']) ):
-			$coupon = get_coupon($_GET['coupon']);
+		elseif( isset($GET['coupon']) ):
+			// Pass: Form Page
+			$coupon = get_coupon_by_code($_GET['coupon']);
 
-			if( $coupon == NULL ):
-				// Error: Coupon doesn't exists
-				echo 'Error: Coupon doesn\'t exists';
-			else:
-				// Pass: Coupon exists and it's valid
+			if( $coupon != NULL ):
+				// Pass: Valid coupon
 				$provider = get_provider_by_coupon($coupon->code);
 ?>
-	<form method="post" action="?coupon=<?php $coupon->code ?>">
-		<strong><?php echo $provider->name ?></strong>
-		<label for="password">Password:</label> <input type="password" name="password" id="password" placeholder="Venue Password" required />
-		<input type="submit" name="submit" id="submit" />
+	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+		<p>
+			<strong>Product Name:</strong><span><?php echo $product->post_name ?></span>
+		</p>
+		<p>
+			<label for="code">Code:</label>
+			<input type="text" name="code" id="code" readonly="readonly" value="<?php $coupon->code ?>" />
+		</p>
+		<p>
+			<label for="password">Password:</label>
+			<input type="password" name="password" id="password" required />
+		</p>
+		<p>
+			<input type="submit" name="submit" id="submit" value="Use Coupon" />
+		</p>
 	</form>
 <?php
+			else:
+				// Error: Coupon doesn't exists or is invalid
+				echo 'Error: Coupon doesn\'t exists or is invalid';
 			endif;
 		else:
-				// Error: Coupon information isn't set
-				echo 'Error: Coupon information isn\'t set';
-?>
-<?php
+			// Error: No coupon code setup
+			echo 'Error: No coupon code is setup';
 		endif;
 	}
 }

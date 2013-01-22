95738327feb47e14e021057818c00cd28d6b0f7d
Irving Kcam <xploit29@gmail.com>
2013-01-22 11:24:46 -0500

    Invalid coupon info

--- includes/shortcodes/coupons.php
+++ includes/shortcodes/coupons.php
@@ -22,6 +22,7 @@ Class shortcode_coupons{
 					// Show coupon information.
 ?>
 	<h3 id="coupon">Coupon Information</h3>
+	<p>
 	<table class="form-table">
 	<tbody>
 		<tr valign="top">
@@ -46,10 +47,12 @@ Class shortcode_coupons{
 		</tr>
 	</tbody>
 	</table>	
+	</p>
 <?php
 					$order = get_order( $coupon->order_id );
 ?>
 	<h3 id="client">Client Information</h3>
+	<p>
 	<table class="form-table">
 	<tbody>
 		<tr valign="top">
@@ -66,10 +69,12 @@ Class shortcode_coupons{
 		</tr>
 	</tbody>
 	</table>
+	</p>
 <?php
 					$product = get_post( $order->post_id );
 ?>
 	<h3 id="product">Product Information</h3>
+	<p>
 	<table class="form-table">
 	<tbody>
 		<tr valign="top">
@@ -90,10 +95,12 @@ Class shortcode_coupons{
 		</tr>
 	</tbody>
 	</table>
+	</p>
 <?php
 					$transaction = get_transaction_by_order($order->ID);
 ?>
 	<h3 id="transaction">Transaction Information</h3>
+	<p>
 	<table class="form-table">
 	<tbody>
 		<tr valign="top">
@@ -114,12 +121,115 @@ Class shortcode_coupons{
 		</tr>
 	</tbody>
 	</table>
+	</p>
 <?php
 					// Set coupon as active.
 					Coupon::set_active($coupon->ID);
 				else:
 					// Error: Invalid coupon.
-					echo 'This coupon was already used.';
+?>
+	<h3 id="coupon">Coupon Information</h3>
+	<p>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">Code:</th>
+			<td><?php echo $coupon->code ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Status:</th>
+			<td><?php echo get_coupon_status($coupon->ID) ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Usage Date:</th>
+			<td>
+<?php
+	if( $coupon->status == 0 ):
+		echo 'None yet';
+	else:
+		echo date( 'H:i m/d/y', $coupon->usage_date );
+	endif;
+?>
+			</td>
+		</tr>
+	</tbody>
+	</table>	
+	</p>
+<?php
+					$order = get_order( $coupon->order_id );
+?>
+	<h3 id="client">Client Information</h3>
+	<p>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">First Name:</th>
+			<td><?php echo $order->first_name ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Last Name:</th>
+			<td><?php echo $order->last_name ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Email:</th>
+			<td><?php echo $order->email ?></td>
+		</tr>
+	</tbody>
+	</table>
+	</p>
+<?php
+					$product = get_post( $order->post_id );
+?>
+	<h3 id="product">Product Information</h3>
+	<p>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">Product Name:</th>
+			<td><?php echo $product->post_title ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Product URL:</th>
+			<td><a href="<?php echo get_permalink($product->ID) ?>" target="_blank"><?php echo get_permalink($product->ID) ?></a></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Product Price:</th>
+			<td>$<?php echo $order->amount ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Buy Date:</th>
+			<td><?php echo date('H:i m-d-Y', $order->date_time) ?></td>
+		</tr>
+	</tbody>
+	</table>
+	</p>
+<?php
+					$transaction = get_transaction_by_order($order->ID);
+?>
+	<h3 id="transaction">Transaction Information</h3>
+	<p>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">Amount:</th>
+			<td><?php echo $transaction->amount ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Transaction ID:</th>
+			<td><?php echo $transaction->txn_id ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Date/Time</th>
+			<td><?php echo date('H:i m/d/Y', $order->date_time) ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Status</th>
+			<td><?php echo get_transaction_status($transaction->ID) ?></td>
+		</tr>
+	</tbody>
+	</table>
+	</p>
+<?php
 				endif;
 			else:
 				// Error: Wrong password

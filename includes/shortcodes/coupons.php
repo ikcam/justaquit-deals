d1c1df1a57a43507f91da13ae742e9d00632576b
Irving Kcam <xploit29@gmail.com>
2013-01-22 10:28:29 -0500

    Coupon information

--- includes/shortcodes/coupons.php
+++ includes/shortcodes/coupons.php
@@ -20,7 +20,100 @@ Class shortcode_coupons{
 				// Verify if is a valid coupon.
 				if( $coupon->status == 0 )
 					// Show coupon information.
-					echo 'This is a valid coupon.';
+?>
+	<h3 id="coupon">Coupon Information</h3>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">Code:</th>
+			<td><?php echo $coupon->code ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Status:</th>
+			<td><?php echo get_coupon_status($coupon->ID) ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Usage Date:</th>
+			<td>
+<?php
+	if( $coupon->status == 0 )
+		echo 'None yet';
+	else
+		echo date( 'H:i m/d/y', $coupon->usage_date );
+?>
+			</td>
+		</tr>
+	</tbody>
+	</table>	
+<?php
+					$order = get_order( $coupon->order_id );
+?>
+	<h3 id="client">Client Information</h3>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">First Name:</th>
+			<td><?php echo $order->first_name ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Last Name:</th>
+			<td><?php echo $order->last_name ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Email:</th>
+			<td><?php echo $order->email ?></td>
+		</tr>
+	</tbody>
+	</table>
+<?php
+					$product = get_post( $order->post_id );
+?>
+	<h3 id="product">Product Information</h3>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">Product Name:</th>
+			<td><?php echo $product->post_title ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Product URL:</th>
+			<td><a href="<?php echo get_permalink($product->ID) ?>" target="_blank"><?php echo get_permalink($product->ID) ?></a></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Product Price:</th>
+			<td>$<?php echo $order->amount ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Buy Date:</th>
+			<td><?php echo date('H:i m-d-Y', $order->date_time) ?></td>
+		</tr>
+	</tbody>
+	</table>
+<?php
+					$transaction = get_transaction_by_order($order->ID);
+?>
+	<h3 id="transaction">Transaction Information</h3>
+	<table class="form-table">
+	<tbody>
+		<tr valign="top">
+			<th scope="row">Amount:</th>
+			<td><?php echo $transaction->amount ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Transaction ID:</th>
+			<td><?php echo $transaction->txn_id ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Date/Time</th>
+			<td><?php echo date('H:i m/d/Y', $order->date_time) ?></td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">Status</th>
+			<td><?php echo get_transaction_status($transaction->ID) ?></td>
+		</tr>
+	</tbody>
+	</table>
+<?php
 					// Set coupon as active.
 					Coupon::set_active($coupon->ID);
 				else:
